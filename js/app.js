72b173244413cffa09d277fc7e896776592961bd
kingalifgit <infinix.alif@gmail.com>
2023-10-29 19:21:22 +0800

    Tambahin beberapa fitur dan responsive nav

--- js/app.js
+++ js/app.js
@@ -19,12 +19,12 @@ window.onscroll = function () {
   const fix = nav.offsetTop;
 
   if (window.pageYOffset > fix) {
-    nav.classList.add("fixed", "w-full", "left-0", "top-0", "rounded-none", "bg-slate-900", "bg-opacity-80", "fix-nav")
+    nav.classList.add("fixed", "w-full", "left-0", "top-0", "mt-3", "bg-opacity-80","rounded-b-lg", "rounded-t-none" ,"fix-nav", "right-0")
     nav.classList.remove('relative')
     navMenu.classList.add("mr-2")
   } else {
     navMenu.classList.remove("mr-2")
     nav.classList.add('relative')
-    nav.classList.remove("fixed", "fix-nav", "bg-opacity-80", "rounded-none")
+    nav.classList.remove("fixed", "fix-nav", "bg-opacity-80" ,"rounded-b-lg", "rounded-t-none")
   }
 }
